45c555784279d79cf17b4b4b7115d54bc57fb4c5
Jun Dev <jun.dev2001@gmail.com>
2026-02-01 22:48:05 +0700

    feat(entity): add user entity

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { createUUIDv7 } from "src/common";
+
+export class User {
+    constructor(
+        public readonly id: string,
+        public readonly name: string,
+        public readonly email: string,
+        public readonly hashPassword: string,
+    ) { }
+
+    static create(props: { name: string, email: string, hashPassword: string }): User {
+        if (!props.email.includes('@'))
+            throw new Error('Invalid email');
+
+        if (!props.name || props.name.trim().length === 0)
+            throw new Error('Name is required');
+
+        return new User(createUUIDv7(), props.name.trim(), props.email.trim(), props.hashPassword.trim());
+    }
+}
