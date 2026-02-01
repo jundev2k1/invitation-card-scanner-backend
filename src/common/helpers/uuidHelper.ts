20fb0f736bb5eda4b9e75ef5b746ae8655274981
Jun Dev <jun.dev2001@gmail.com>
2026-02-01 22:19:17 +0700

    feat(common): add uuid generic helper

---
+++ src/common/helpers/uuidHelper.ts
@@ -0,0 +1,10 @@
+import { randomUUID } from "crypto"
+import { uuidv7 } from "uuidv7";
+
+export const createUUIDv4 = () => {
+    return randomUUID();
+}
+
+export const createUUIDv7 = () => {
+    return uuidv7();
+}
