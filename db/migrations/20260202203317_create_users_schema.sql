4bf59febb551b45b6dc6e326f4969c5658e72f36
Jun Dev <jun.dev2001@gmail.com>
2026-02-05 02:08:55 +0700

    refactor(migration): change detail for the user table and create/update functions

--- db/migrations/20260202203317_create_users_schema.sql
+++ db/migrations/20260202203317_create_users_schema.sql
@@ -3,20 +3,22 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  phone_number VARCHAR(50) NOT NULL DEFAULT '',
   nickname VARCHAR(100) NOT NULL DEFAULT '',
   password_hash VARCHAR(255) NOT NULL,
   sex CHAR(1) CHECK (sex IN ('M', 'F', 'O')) DEFAULT 'O',
-  bio TEXT DEFAULT '',
-  avatar_url VARCHAR(255) DEFAULT '',
-  is_active BOOLEAN DEFAULT TRUE,
+  bio VARCHAR(4000) NOT NULL DEFAULT '',
+  avatar_url VARCHAR(255) NOT NULL DEFAULT '',
+  "status" SMALLINT DEFAULT 0,
+  "role" VARCHAR(20) CHECK ("role" IN ('ROOT', 'ADMIN', 'STAFF')) NOT NULL DEFAULT 'STAFF',
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_username ON users(username);
 CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
-CREATE INDEX IF NOT EXISTS idx_users_is_active ON users(is_active);
+CREATE INDEX IF NOT EXISTS idx_users_status ON users("status");
 
 CREATE OR REPLACE FUNCTION search_users_by_criteria(
   p_keyword VARCHAR,
@@ -24,13 +26,14 @@ CREATE OR REPLACE FUNCTION search_users_by_criteria(
   p_offset INT
 )
 RETURNS TABLE (
-  id UUID, username VARCHAR,
+  id UUID,
+  username VARCHAR,
   email VARCHAR,
-  nickname VARCHAR, 
+  nickname VARCHAR,
+  phone_number VARCHAR,
   sex CHAR(1),
-  bio TEXT,
   avatar_url VARCHAR,
-  is_active BOOLEAN, 
+  "status" SMALLINT,
   created_at TIMESTAMPTZ,
   updated_at TIMESTAMPTZ,
   total_count BIGINT
@@ -42,10 +45,10 @@ BEGIN
           u.username,
           u.email,
           u.nickname,
+          u.phone_number,
           u.sex,
-          u.bio,
           u.avatar_url,
-          u.is_active,
+          u.status,
           u.created_at,
           u.updated_at,
           COUNT(*) OVER() AS total_count
@@ -105,14 +108,27 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION check_user_exists_by_email(p_email VARCHAR)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN EXISTS(SELECT 1 FROM users WHERE email = p_email);
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION create_user(
   p_id UUID,
   p_username VARCHAR,
   p_email VARCHAR,
   p_nickname VARCHAR,
+  p_phone_number VARCHAR,
   p_password_hash VARCHAR,
   p_sex CHAR,
-  p_bio VARCHAR)
+  p_bio VARCHAR,
+  p_avatar_url VARCHAR,
+  p_status SMALLINT,
+  p_role VARCHAR)
 RETURNS VOID
 LANGUAGE plpgsql
 AS $$
@@ -122,9 +138,13 @@ BEGIN
     username,
     email,
     nickname,
+    phone_number,
     password_hash,
     sex,
     bio,
+    avatar_url,
+    "status",
+    "role",
     created_at,
     updated_at)
   VALUES (
@@ -132,9 +152,13 @@ BEGIN
     p_username,
     p_email,
     p_nickname,
+    p_phone_number,
     p_password_hash,
     p_sex,
     p_bio,
+    p_avatar_url,
+    p_status,
+    p_role,
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP);
 END;
@@ -142,21 +166,28 @@ $$;
 
 CREATE OR REPLACE FUNCTION update_user(
   p_id UUID,
+  p_email VARCHAR,
   p_nickname VARCHAR,
+  p_phone_number VARCHAR,
+  p_password_hash VARCHAR,
+  p_sex CHAR,
   p_bio VARCHAR,
   p_avatar_url VARCHAR,
-  p_is_active BOOLEAN)
+  p_status SMALLINT)
 RETURNS VOID
 LANGUAGE plpgsql
 AS $$
 BEGIN
-  UPDATE users
-  SET nickname = p_nickname,
-    bio = p_bio,
-    avatar_url = p_avatar_url,
-    is_active = p_is_active,
-    updated_at = CURRENT_TIMESTAMP
-  WHERE id = p_id;
+  UPDATE  users
+     SET  nickname = p_nickname,
+          phone_number = p_phone_number,
+          password_hash = p_password_hash,
+          sex = p_sex,
+          bio = p_bio,
+          avatar_url = p_avatar_url,
+          "status" = p_status,
+          updated_at = CURRENT_TIMESTAMP
+   WHERE  id = p_id;
 END;
 $$;
 
@@ -175,3 +206,4 @@ DROP FUNCTION IF EXISTS get_user_by_username;
 DROP FUNCTION IF EXISTS get_user_by_email;
 DROP FUNCTION IF EXISTS search_users_by_criteria;
 DROP FUNCTION IF EXISTS check_user_exists_by_username;
+DROP FUNCTION IF EXISTS check_user_exists_by_email;
