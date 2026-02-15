7d3673dbe13eef316408dc5933357c282357d08d
Jun Dev <jun.dev2001@gmail.com>
2026-02-16 01:18:41 +0700

    fix(update user): missing DI

--- src/application/features/users/user.cqrs.module.ts
+++ src/application/features/users/user.cqrs.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { RepositoryModule } from "src/infrastracture/repositories/repository.module";
 import { ApproveUserHandler } from "./commands/approve-user/approve-user.handler";
+import { UpdateUserHandler } from "./commands/update-user/update-user.handler";
 import { GetUserDetailHandler } from "./queries/get-user-detail/get-user-detail.handler";
 import { GetUserListHandler } from "./queries/get-user-list/get-user-list.handler";
 import { GetUserStatusCountHandler } from "./queries/get-user-status-count/get-status-count.handler";
@@ -11,7 +12,8 @@ const queries = [
   GetUserStatusCountHandler,
 ];
 const commands = [
-  ApproveUserHandler
+  ApproveUserHandler,
+  UpdateUserHandler,
 ];
 
 @Module({
