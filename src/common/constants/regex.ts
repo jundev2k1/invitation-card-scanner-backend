85db774901d738e0e70f87b37c5c44c26c3b596d
Jun Dev <jun.dev2001@gmail.com>
2026-02-16 00:57:06 +0700

    fix(phone format): change regex to check phone number so that allowed more type of format phone number

--- src/common/constants/regex.ts
+++ src/common/constants/regex.ts
@@ -1,2 +1,2 @@
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const phoneRegex = /^(?:\+84|0)([35789]|2[0-9])(?:\s?\d){8}$/g;
+export const phoneRegex = /^\+?\d(?:[\s]?\d){2,14}$/; 
